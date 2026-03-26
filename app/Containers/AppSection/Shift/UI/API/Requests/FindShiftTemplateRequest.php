60c1febdb7ab9f792480250ce8c2053fc0a87d01
TuanDv <tuan.dang@flashtech.asia>
2026-03-26 20:07:36 +0700

    update

--- app/Containers/AppSection/Shift/UI/API/Requests/FindShiftTemplateRequest.php
+++ app/Containers/AppSection/Shift/UI/API/Requests/FindShiftTemplateRequest.php
@@ -10,9 +10,7 @@ final class FindShiftTemplateRequest extends ParentRequest
 
     public function rules(): array
     {
-        return [
-            'id' => ['required'],
-        ];
+        return [];
     }
 
     public function authorize(): bool
