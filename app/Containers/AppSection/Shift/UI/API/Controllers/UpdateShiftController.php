cfd7164bd3a8892e48ea6dc91726444afad36cea
TuanDv <tuan.dang@flashtech.asia>
2026-04-01 17:23:43 +0700

    update

--- app/Containers/AppSection/Shift/UI/API/Controllers/UpdateShiftController.php
+++ app/Containers/AppSection/Shift/UI/API/Controllers/UpdateShiftController.php
@@ -2,17 +2,19 @@
 
 namespace App\Containers\AppSection\Shift\UI\API\Controllers;
 
+use Apiato\Support\Facades\Response;
 use App\Containers\AppSection\Shift\Actions\UpdateShiftAction;
 use App\Containers\AppSection\Shift\UI\API\Requests\UpdateShiftRequest;
 use App\Containers\AppSection\Shift\UI\API\Transformers\ShiftTransformer;
 use App\Ship\Parents\Controllers\ApiController;
+use Illuminate\Http\JsonResponse;
 
 final class UpdateShiftController extends ApiController
 {
-    public function __invoke(UpdateShiftRequest $request): array
+    public function __invoke(UpdateShiftRequest $request): JsonResponse
     {
         $shift = app(UpdateShiftAction::class)->run($request->id, $request->validated());
 
-        return $this->transform($shift, ShiftTransformer::class);
+        return Response::create($shift, ShiftTransformer::class)->ok();
     }
 }
