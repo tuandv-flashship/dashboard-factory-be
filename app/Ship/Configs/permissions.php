e19b6b0b1a5a6fdc1a5930117c9fc1695f235976
TuanDv <tuan.dang@flashtech.asia>
2026-04-21 21:49:10 +0700

    update

--- app/Ship/Configs/permissions.php
+++ app/Ship/Configs/permissions.php
@@ -5,26 +5,26 @@ return [
         'name' => 'System',
         'flag' => 'core.system',
     ],
-    [
-        'name' => 'CMS',
-        'flag' => 'core.cms',
-    ],
+    // [
+    //     'name' => 'CMS',
+    //     'flag' => 'core.cms',
+    // ],
     [
         'name' => 'Settings',
         'flag' => 'settings.common',
     ],
-    [
-        'name' => 'Cronjob',
-        'flag' => 'systems.cronjob',
-        'parent_flag' => 'core.system',
-    ],
-    [
-        'name' => 'Tools',
-        'flag' => 'core.tools',
-    ],
-    [
-        'name' => 'Import/Export Data',
-        'flag' => 'tools.data-synchronize',
-        'parent_flag' => 'core.tools',
-    ],
+    // [
+    //     'name' => 'Cronjob',
+    //     'flag' => 'systems.cronjob',
+    //     'parent_flag' => 'core.system',
+    // ],
+    // [
+    //     'name' => 'Tools',
+    //     'flag' => 'core.tools',
+    // ],
+    // [
+    //     'name' => 'Import/Export Data',
+    //     'flag' => 'tools.data-synchronize',
+    //     'parent_flag' => 'core.tools',
+    // ],
 ];
